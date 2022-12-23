cfa9376586ee72d87f572f0c4dd3dfaf28ee2935
touhf <tobytubar@gmail.com>
2022-12-23 17:25:46 +0500

    setup files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'rcoc',
+  packages = ['rcoc'],
+  version = 'v1.0.0',
+  license='MIT',
+  description = 'RCOC - Random Country Or City name generator',
+  author = 'touhf',
+  author_email = 'tobytubar@gmail.com',
+  url = 'https://github.com/touhf/rcoc',
+  download_url = 'https://github.com/touhf/rcoc/archive/refs/tags/v1.0.0.tar.gz',
+  keywords = ['country', 'city', 'generator'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
