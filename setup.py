b915bdfd0916087e4cd55a79bedf843ba5f57f77
touhf <tobytubar@gmail.com>
2023-01-05 18:48:14 +0500

    changed config

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
   name = 'rcoc',
   packages = ['rcoc'],
-  package_dir = {'rcoc': 'rcoc'},
-  package_data = {'rcoc': ['data/*.csv']},
-  version = '1.1.9',
+  package_dir = {'': 'rcoc'},
+  package_data = {'': ['*.csv']},
+  version = '1.1.10',
   license='MIT',
   description = 'RCOC - Random Country Or City name generator',
   author = 'touhf',
@@ -19,7 +19,7 @@ setup(
   url = 'https://github.com/touhf/rcoc',
   long_description=long_description,
   long_description_content_type="text/markdown",
-  download_url = 'https://github.com/touhf/rcoc/archive/refs/tags/1.1.9.tar.gz',
+  download_url = 'https://github.com/touhf/rcoc/archive/refs/tags/1.1.10.tar.gz',
   keywords = ['country', 'city', 'generator'],
   install_requires=[],
   classifiers=[
